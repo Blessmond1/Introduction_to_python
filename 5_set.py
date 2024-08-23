cb73003076de8b07e06886cb0a86fb364514a45d
blessmond1 <thankgodadesuyi@gmail.com>
2024-08-23 13:38:18 +0100

    creating my set

---
+++ 5_set.py
@@ -0,0 +1,19 @@
+mySetItems = {"bag", "cap", "shoe", "watch", "book", "laptop"}
+
+# print length of items
+print (len(mySetItems))
+
+# iterate over the items
+for x in mySetItems:
+    print (x)
+
+# add set items to,
+print (mySetItems.add("Visa"))
+
+
+
+
+
+
+
+print (mySetItems)
