c7b0929fb3347745ba433b73d844ac2f44ee9d92
blessmond1 <thankgodadesuyi@gmail.com>
2024-08-23 13:37:01 +0100

    creating my dic

---
+++ 6_dict.py
@@ -0,0 +1,13 @@
+mydata = {
+   " surname": "thankgod",
+   "firstname": "blessed",
+   "color": "green"   
+}
+
+# add to database
+mydata["food"] = "rice"
+
+# remove from database
+mydata.pop("firstname") 
+
+print (mydata)
