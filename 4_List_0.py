874a10ec2223787cee6e6505b965be175c1a9527
blessmond1 <thankgodadesuyi@gmail.com>
2024-08-23 11:23:08 +0100

    creating my tech list

--- 4_List_0.py
+++ 4_List_0.py
@@ -1,7 +1,33 @@
-myTechList = ["Pythons", "Javascript", "Java","ReactJs","C++","PHP","Bootstrap"]
-print (myTechList)
+myTechList = ["Python", "Javascript", "Java","ReactJs","C++","PHP","Bootstrap"]
+myTechList2 = ["AWS"]
 
 # Adding a new technology
 
 myTechList.append("HTML")
+
+# print a technology in index 2
+print (myTechList[2])
+
+#insert a new technology
+myTechList.insert(2, "C")
+myTechList.insert( 5, "Cybersecurity")
+
+# extend the technology
+myTechList.extend(myTechList2)
+
+#remove the technology
+myTechList.remove("Cybersecurity")
+
+#pop the technology
+myTechList.pop(2)
+
+#delete the technology
+del(myTechList[6])
+
+
+# print the length of the technology
+print (len(myTechList))
+
+
+
 print (myTechList)
