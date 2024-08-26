b72157d9a31ca82e2cf2238ffdd82d2c05ae340f
blessmond1 <thankgodadesuyi@gmail.com>
2024-08-27 00:30:50 +0100

    A1age.py

---
+++ A1age.py
@@ -0,0 +1,9 @@
+# Write a Python program that asks the user for their age. If the user is 18 or older, 
+# print "You are eligible to vote." Otherwise, print "You are not eligible to vote."
+Age = 18
+Age = int(input("how old you are"))
+
+if Age >= 18:
+    print ("Age is eligible to vote")
+else:
+    print ("Age is not eligible to vote")
