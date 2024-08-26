4adf918ade1a02147d05253df418c269c0785096
blessmond1 <thankgodadesuyi@gmail.com>
2024-08-27 00:44:00 +0100

    A4divi.py

---
+++ A4divi.py
@@ -0,0 +1,13 @@
+# Write a Python program that checks if a number entered by the user is either
+# divisible by 3 or divisible by 5, and prints the appropriate message.
+number = float(input("enter a number: "))
+
+if number % 3 == 0 or number % 5 == 0:
+    
+    print("number is divisible by 3 or 5.")
+else:
+    print("number is not divisible by 3 or 5.")
+
+
+
+
