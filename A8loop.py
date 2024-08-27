516c7085d9a31da9bba9267ea1530a932b1925ae
blessmond1 <thankgodadesuyi@gmail.com>
2024-08-27 01:07:39 +0100

    A8loop.py

---
+++ A8loop.py
@@ -0,0 +1,9 @@
+#Write a Python program that uses a while loop to print numbers from 1 to 10. 
+#Use a break statement to stop the loop when the number 7 is reached.
+
+num = 1
+while num <= 10:
+    if num == 7:
+        break
+    print (num)
+    num += 1    
